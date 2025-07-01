b893032870a2fe83abd985ba3a577b7e4aad3122
sandjonyves <sandjonyves@gmail.com>
2025-07-02 00:27:29 +0200

    corect cors

--- automate_api/settings.py
+++ automate_api/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = ['https://tp-automate-frontend.onrender.com']
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
