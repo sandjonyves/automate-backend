64a62e3205b1ce7c2dddcc5c75708f938bce0c16
sandjonyves <sandjonyves@gmail.com>
2025-07-02 07:14:35 +0200

    test

--- automate_api/settings.py
+++ automate_api/settings.py
@@ -135,8 +135,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['https://tp-automate-frontend.onrender.com']
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://tp-automate-frontend.onrender.com",
+    "http://localhost:3000",
+]
 
 
 STATIC_URL = '/static/'
