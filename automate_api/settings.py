834ba6f3baacb2ca3ffb40e8d987f3d597f3bcf0
sandjonyves <sandjonyves@gmail.com>
2025-07-02 07:06:25 +0200

    test

--- automate_api/settings.py
+++ automate_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$e6*7kwkunpvjxg_-na*n_cv&h(d=&ebdne*zv&m8%$%vzay^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://tp-automate-latest.onrender.com']
 
 
 
@@ -128,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
